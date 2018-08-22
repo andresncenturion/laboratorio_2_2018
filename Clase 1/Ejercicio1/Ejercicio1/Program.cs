795135a30bb54d54c7b388cc99fffedcf50bea92
alumno <alumno>
2018-08-22 20:57:30 -0300

    Clase 1

---
+++ Clase 1/Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int var = -1;
+            int flagMax = 0;
+            int flagMin = 0;
+            int length = 5;
+            int i;
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            float cont = 0;
+            float acum = 0;
+            float prom = -1;
+
+            for (i = 0; i < length; i++)
+            {
+                Console.Write("Ingrese un numero: ");
+                var = int.Parse(Console.ReadLine());
+                cont++;
+                acum += var;
+                if (var > max || flagMax == 0)
+                {
+                    flagMax = 1;
+                    max = var;
+                }
+                if (var < min || flagMin == 0)
+                {
+                    flagMin = 1;
+                    min = var;
+                }
+            }
+            prom = acum / cont;
+            Console.Write("Maximo: ");
+            Console.WriteLine(max);
+            Console.Write("Minimo: ");
+            Console.WriteLine(min);
+            Console.Write("Promedio: ");
+            Console.Write(prom);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Clase 1/Ejercicio1/Ejercicio2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int var = 0;
+            double cuadrado;
+            double cubo;
+
+            Console.Write("Ingrese un numero: ");
+            var = int.Parse(Console.ReadLine());
+            if (var > 0)
+            {
+                cuadrado = Math.Pow(var, 2);
+                Console.Write("Cuadrado: ");
+                Console.WriteLine(cuadrado);
+                cubo = Math.Pow(var, 3);
+                Console.Write("Cubo: ");
+                Console.WriteLine(cubo);
+            }
+            else
+            {
+                Console.Write("ERROR. ¡Reingresar numero!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
