1014908f38d07047ccea1f35c8710f07ca570af8
alumno <alumno>
2018-08-30 18:26:28 -0300

    Clase 3 Ejercicio 16

---
+++ Clase 3/Ejercicio16/Ejercicio16/Alumno.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Ejercicio16
+{
+    public class Alumno
+    {
+        private byte _nota1;
+        private byte _nota2;
+        private float _notaFinal;
+        public string apellido;
+        public int legajo;
+        public string nombre;
+
+        public void CalcularFinal ()
+        {
+            this._notaFinal = -1;
+            Random numero = new Random();
+
+            if (this._nota1 >= 4 & this._nota2 >= 4)
+            {
+                this._notaFinal = numero.Next();
+            }
+        }
+
+        public void Estudiar (byte NotaUno, byte NotaDos)
+        {
+            this._nota1 = NotaUno;
+            this._nota2 = NotaDos;
+        }
+
+        public string Mostrar ()
+        {
+            if(this._notaFinal != -1)
+            {
+                Console.WriteLine ("Nota final: "+_notaFinal)
+            }
+        }
+
+        public Alumno(byte nota1, byte nota2, float notaFinal, string apellido, int legajo, string nombre)
+        {
+            _nota1 = nota1;
+            _nota2 = nota2;
+            _notaFinal = notaFinal;
+            this.apellido = apellido;
+            this.legajo = legajo;
+            this.nombre = nombre;
+        }
+    }
+}
---
+++ Clase 3/Ejercicio16/Ejercicio16/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio16
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            Alumno Alumno1 = new Alumno();
+            Alumno Alumno2 = new Alumno();
+            Alumno Alumno3 = new Alumno();
+
+            Alumno1.nombre = Console.ReadLine();
+            Alumno1.apellido = Console.ReadLine();
+            Alumno1.legajo = Console.ReadLine();
+
+            Alumno2.nombre = Console.ReadLine();
+            Alumno2.apellido = Console.ReadLine();
+            Alumno2.legajo = Console.ReadLine();
+
+            Alumno3.nombre = Console.ReadLine();
+            Alumno3.apellido = Console.ReadLine();
+            Alumno3.legajo = Console.ReadLine();            
+
+            Alumno1.Estudiar(4, 5);
+            Alumno2.Estudiar(7, 6);
+            Alumno3.Estudiar(9, 7);
+
+            Alumno1.CalcularFinal();
+            Alumno2.CalcularFinal();
+            Alumno3.CalcularFinal();
+
+            Alumno1.Mostrar();
+            Alumno2.Mostrar();
+            Alumno3.Mostrar();
+        }
+    }
+}
