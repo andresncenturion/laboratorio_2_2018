f87552d8deee7b2666d7edfb7c4f383db058f529
alumno <alumno>
2018-09-20 22:01:36 -0300

    Ej 37

--- 09- Herencia/CentralTelefonica/CentralitaHerencia/Centralita.cs
+++ 09- Herencia/CentralTelefonica/CentralitaHerencia/Centralita.cs
@@ -8,6 +8,90 @@ namespace CentralitaHerencia
 {
     class Centralita
     {
-        private List<Llamada> listaDeLlamadas
+        private List<Llamada> listaDeLlamadas;
+        private string razonSocial;
+
+        public float GananciasPorLocal
+        {
+            get
+            {                
+                
+            }
+        }
+
+        public float GananciasPorProvincia
+        {
+            get
+            {
+
+            }
+        }
+
+        public float GananciasPorTotal
+        {
+            get
+            {
+
+            }
+        }
+
+        public List<Llamada> Llamadas
+        {
+            get
+            {
+
+            }
+        }
+
+        private float CalcularGanancia(Llamada.TipoLlamada tipo)
+        {
+            float valorRecaudado;
+            float valorTotal = 0;
+
+            foreach (Llamada llamada in listaDeLlamadas)
+            {
+                switch (tipo)
+                {
+                    case Local:
+                        if (llamada is Local)
+                        {
+                            valorRecaudado = ((Local)llamada).CostoLlamada;
+                            valorTotal += valorRecaudado;
+                        }
+                        break;
+                    case Provincial:
+                        if (llamada is Provincial)
+                        {
+                            valorRecaudado = ((Provincial)llamada).CostoLlamada;
+                            valorTotal += valorRecaudado;
+                        }
+                        break;
+                    case Todas:
+                        valorRecaudado = valorTotal;
+                        break;                        
+                }                
+            }
+            return valorRecaudado;
+        }
+
+        public Centralita()
+        {
+
+        }
+
+        public Centralita(string nombreEmpresa)
+        {
+
+        }
+
+        public string Mostrar()
+        {
+
+        }
+
+        public void OrdenarLlamadas()
+        {
+
+        }
     }
 }
