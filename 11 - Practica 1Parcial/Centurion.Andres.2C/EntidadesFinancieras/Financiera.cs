75222362ae76ea0bdf1d2b2683a57861c45b9661
alumno <alumno>
2018-10-03 22:09:21 -0300

    Practica re parcial

--- 11 - Practica 1Parcial/Centurion.Andres.2C/EntidadesFinancieras/Financiera.cs
+++ 11 - Practica 1Parcial/Centurion.Andres.2C/EntidadesFinancieras/Financiera.cs
@@ -5,9 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using PrestamosPersonales;
 
-namespace EntidadFinancier
+namespace EntidadFinanciera
 {
-    class Financiera
+    public class Financiera
     {
         private List<Prestamo> listaDePrestamos;
         private string razonSocial;
@@ -28,6 +28,30 @@ namespace EntidadFinancier
             }
         }
 
+        public float InteresesEnDolares
+        {
+            get
+            {
+                return this.CalcularInteresGanados(TipoDePrestamo.Dolares);
+            }
+        }
+
+        public float InteresesEnPesos
+        {
+            get
+            {
+                return this.CalcularInteresGanados(TipoDePrestamo.Pesos);
+            }
+        }
+
+        public float InteresesTotales
+        {
+            get
+            {
+                return this.CalcularInteresGanados(TipoDePrestamo.Todos);
+            }
+        }
+
         private Financiera()
         {
             this.listaDePrestamos = new List<Prestamo>();
@@ -38,9 +62,55 @@ namespace EntidadFinancier
             this.razonSocial = razonSocial;
         }
 
+        private float CalcularInteresGanados(TipoDePrestamo tipoPrestamo)
+        {
+            float intereses = 0;            
+
+            foreach (Prestamo prestamo in ListaDePrestamos)
+            {
+                switch(tipoPrestamo)
+                {
+                    case TipoDePrestamo.Dolares:
+                        if (prestamo is PrestamoDolar)
+                        {
+                            intereses += ((PrestamoDolar)prestamo).Interes;
+                        }                        
+                        break;
+                    case TipoDePrestamo.Pesos:
+                        if (prestamo is PrestamoPesos)
+                        {
+                            intereses += ((PrestamoPesos)prestamo).Interes;
+                        }
+                        break;
+                    case TipoDePrestamo.Todos:
+                        {
+                            intereses += ((PrestamoDolar)prestamo).Interes + ((PrestamoPesos)prestamo).Interes;
+                        }
+                        break;
+                }
+            }
+            return intereses;
+        }
+
         public string Mostrar()
         {
+           
+        }
 
+        public static explicit operator string(Financiera financiera)
+        {
+            StringBuilder sb = new StringBuilder();
+            
+
+            sb.AppendFormat("Razon social: {0}", financiera.razonSocial);
+            foreach (Prestamo prestamo in financiera.listaDePrestamos)
+            {
+                if(prestamo is PrestamoDolar)
+                {
+
+                }
+            }        
+            sb.AppendFormat("Intereses ganados: {1}", )
         }
     }
 }
